2d4759487b2c1b672d89d40a3a2f80e22c7917ee
mangxiao <mangxiao2018@126.com>
2021-10-21 07:10:58 +0800

    dubbo-consumer

---
+++ dubbo-samples-consumer/src/main/java/com/mangxiao/samples/dubbo/consumer/DubboSampleConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.mangxiao.samples.dubbo.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboSampleConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DubboSampleConsumerApplication.class, args);
+    }
+}
